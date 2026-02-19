fbd9ab9b45f0afe6e883816199a8d98e4e94b095
Link-Card <linkcard@linkcard.ca>
2026-02-19 04:38:26 +0000

    Add wave animation to preview - exact copy of public
    Wave/double_wave now animate in editor preview with same
    CSS animation (translate3d parallax, 4 layers, staggered durations).
    Preview = public, no more static waves.

--- resources/views/livewire/profile/partials/preview-transition.blade.php
+++ resources/views/livewire/profile/partials/preview-transition.blade.php
@@ -20,31 +20,53 @@
 @endphp
 
 @if($transition === 'wave')
-    <div style="position: relative; z-index: 5; margin-top: -1px; margin-bottom: -2px; line-height: 0; height: 30px; overflow: hidden;">
+    <style>
+        .pv-waves { position: relative; z-index: 5; width: 100%; height: 30px; margin-top: -1px; margin-bottom: -2px; overflow: hidden; }
+        .pv-waves .pv-parallax > use { animation: pv-wave-move 25s cubic-bezier(.55,.5,.45,.5) infinite; }
+        .pv-waves .pv-parallax > use:nth-child(1) { animation-delay: -2s; animation-duration: 7s; }
+        .pv-waves .pv-parallax > use:nth-child(2) { animation-delay: -3s; animation-duration: 10s; }
+        .pv-waves .pv-parallax > use:nth-child(3) { animation-delay: -4s; animation-duration: 13s; }
+        .pv-waves .pv-parallax > use:nth-child(4) { animation-delay: -5s; animation-duration: 20s; }
+        @keyframes pv-wave-move { 0% { transform: translate3d(-90px,0,0); } 100% { transform: translate3d(85px,0,0); } }
+    </style>
+    <div class="pv-waves">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
              viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto"
              style="display: block; width: 100%; height: 100%;">
             <defs>
                 <path id="pw" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
             </defs>
-            <use xlink:href="#pw" x="48" y="0" fill="{{ $fillAlpha['70'] }}" />
-            <use xlink:href="#pw" x="48" y="3" fill="{{ $fillAlpha['50'] }}" />
-            <use xlink:href="#pw" x="60" y="5" fill="{{ $fillAlpha['50'] }}" />
-            <use xlink:href="#pw" x="48" y="7" fill="{{ $fillColor }}" />
+            <g class="pv-parallax">
+                <use xlink:href="#pw" x="48" y="0" fill="{{ $fillAlpha['70'] }}" />
+                <use xlink:href="#pw" x="48" y="3" fill="{{ $fillAlpha['50'] }}" />
+                <use xlink:href="#pw" x="48" y="5" fill="{{ $fillAlpha['50'] }}" />
+                <use xlink:href="#pw" x="48" y="7" fill="{{ $fillColor }}" />
+            </g>
         </svg>
     </div>
 @elseif($transition === 'double_wave')
-    <div style="position: relative; z-index: 5; margin-top: -1px; margin-bottom: -2px; line-height: 0; height: 35px; overflow: hidden;">
+    <style>
+        .pv-dwaves { position: relative; z-index: 5; width: 100%; height: 35px; margin-top: -1px; margin-bottom: -2px; overflow: hidden; }
+        .pv-dwaves .pv-dparallax > use { animation: pv-dwave-move 25s cubic-bezier(.55,.5,.45,.5) infinite; }
+        .pv-dwaves .pv-dparallax > use:nth-child(1) { animation-delay: -2s; animation-duration: 7s; }
+        .pv-dwaves .pv-dparallax > use:nth-child(2) { animation-delay: -3s; animation-duration: 10s; }
+        .pv-dwaves .pv-dparallax > use:nth-child(3) { animation-delay: -4s; animation-duration: 13s; }
+        .pv-dwaves .pv-dparallax > use:nth-child(4) { animation-delay: -5s; animation-duration: 20s; }
+        @keyframes pv-dwave-move { 0% { transform: translate3d(-90px,0,0); } 100% { transform: translate3d(85px,0,0); } }
+    </style>
+    <div class="pv-dwaves">
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
              viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto"
              style="display: block; width: 100%; height: 100%;">
             <defs>
                 <path id="pdw" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
             </defs>
-            <use xlink:href="#pdw" x="48" y="0" fill="{{ $fillAlpha['70'] }}" />
-            <use xlink:href="#pdw" x="48" y="2" fill="{{ $fillAlpha['50'] }}" />
-            <use xlink:href="#pdw" x="60" y="4" fill="{{ $fillAlpha['50'] }}" />
-            <use xlink:href="#pdw" x="48" y="6" fill="{{ $fillColor }}" />
+            <g class="pv-dparallax">
+                <use xlink:href="#pdw" x="48" y="0" fill="{{ $fillAlpha['70'] }}" />
+                <use xlink:href="#pdw" x="48" y="2" fill="{{ $fillAlpha['50'] }}" />
+                <use xlink:href="#pdw" x="48" y="4" fill="{{ $fillAlpha['50'] }}" />
+                <use xlink:href="#pdw" x="48" y="6" fill="{{ $fillColor }}" />
+            </g>
         </svg>
     </div>
 @elseif($transition === 'arch')
